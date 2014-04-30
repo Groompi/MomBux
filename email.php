25e2a6fd7b6d47dc0fec0b6b6b49a64d415aa142
mooofy <tinytacosftw@gmail.com>
2014-04-30 01:13:35 -0400

    added email
    email added

--- email.php
+++ email.php
@@ -1,7 +1,7 @@
 <?php
 	$service = $_GET['sid'];
 
-	 $to = "twp8888@gmail.com";
+	 $to = "email";
 	 $subject = "$service";
 	 $body = "Mom has asked you to $service\n\nshe has $totalBux bux remaining.";
 	 mail($to, $subject, $body);
