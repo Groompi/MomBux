b3709e410fd030fcc57c9d6c70bc9e544655c555
Tyler <tinytacosftw@gmail.com>
2014-04-29 22:01:38 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,6 +1,6 @@
 <?php
 		try{
-			$handler = new PDO('mysql:host=localhost;dbname=mombux','root','ileatpie1');
+			$handler = new PDO('mysql:host=localhost;dbname=mombux','root','pw');
 			$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(PDOException $e){
 			echo $e->getMessage();
